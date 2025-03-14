c3214d4c03d32c4bbe6d4b78ad7012c913526de6
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-14 18:51:47 +0100

    added class for saving constant values

---
+++ src/main/java/org/simpleaas/nameplate/NameplateConstants.java
@@ -0,0 +1,11 @@
+package org.simpleaas.nameplate;
+
+public class NameplateConstants {
+    public static class nameplate2_0 {
+        public static String uriOfTheProduct = "";
+    }
+
+    public static class nameplate3_0 {
+
+    }
+}
