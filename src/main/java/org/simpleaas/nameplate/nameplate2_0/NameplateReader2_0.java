7c69c27266b00b1ec33ca75f9aa2311945c4a4a3
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-15 12:41:04 +0100

    finished implementation for simple nameplate 2.0 property iin reader class

--- src/main/java/org/simpleaas/nameplate/nameplate2_0/NameplateReader2_0.java
+++ src/main/java/org/simpleaas/nameplate/nameplate2_0/NameplateReader2_0.java
@@ -2,6 +2,7 @@ package org.simpleaas.nameplate.nameplate2_0;
 
 import org.eclipse.digitaltwin.aas4j.v3.model.*;
 import org.simpleaas.exception.SubmodelElementNotFoundException;
+import org.simpleaas.helper.FileModel;
 import org.simpleaas.nameplate.NameplateConstants;
 
 import java.util.HashMap;
@@ -75,7 +76,7 @@ public class NameplateReader2_0 {
                 throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of manufacturer product root is from unexpected type " + element.getClass().getName() + ".");
             }
         } else {
-            throw new SubmodelElementNotFoundException("Unable to find submodel element with semantic identifier of manufacturer product root in the submodel.");
+            return new HashMap<>();
         }
     }
 
@@ -91,7 +92,7 @@ public class NameplateReader2_0 {
                 throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of manufacturer product family is from unexpected type " + element.getClass().getName() + ".");
             }
         } else {
-            throw new SubmodelElementNotFoundException("Unable to find submodel element with semantic identifier of manufacturer product family in the submodel.");
+            return new HashMap<>();
         }
     }
 
@@ -107,7 +108,167 @@ public class NameplateReader2_0 {
                 throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of manufacturer product type is from unexpected type " + element.getClass().getName() + ".");
             }
         } else {
-            throw new SubmodelElementNotFoundException("Unable to find submodel element with semantic identifier of manufacturer product type in the submodel.");
+            return new HashMap<>();
+        }
+    }
+
+    public HashMap<String, String> getOrderCodeOfManufacturer() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optOrderCode = getSubmodelElement(NameplateConstants.Nameplate2_0.orderCodeOfManufacturer, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optOrderCode.isPresent()) {
+            SubmodelElement element = optOrderCode.get();
+
+            if(element instanceof MultiLanguageProperty mlp) {
+                return convertMlpToHashmap(mlp.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of order code of manufacturer is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    public HashMap<String, String> getProductArticleNumberOfManufacturer() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optArticleNumber = getSubmodelElement(NameplateConstants.Nameplate2_0.productArticleNumber, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optArticleNumber.isPresent()) {
+            SubmodelElement element = optArticleNumber.get();
+
+            if(element instanceof MultiLanguageProperty mlp) {
+                return convertMlpToHashmap(mlp.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of product article number of manufacturer is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    public Optional<String> getSerialNumber() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optSerialNumber = getSubmodelElement(NameplateConstants.Nameplate2_0.serialNumber, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optSerialNumber.isPresent()) {
+            SubmodelElement element = optSerialNumber.get();
+
+            if(element instanceof Property p) {
+                return Optional.of(p.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of serial number is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public String getYearOfConstruction() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optConstructionYear = getSubmodelElement(NameplateConstants.Nameplate2_0.yearOfConstruction, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optConstructionYear.isPresent()) {
+            SubmodelElement element = optConstructionYear.get();
+
+            if(element instanceof Property p) {
+                return p.getValue();
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of year of construction is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            throw new SubmodelElementNotFoundException("Unable to find submodel element with semantic identifier of year of construction in the submodel.");
+        }
+    }
+
+    public Optional<String> getDateOfManufacture() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optManuDate = getSubmodelElement(NameplateConstants.Nameplate2_0.dateOfManufacture, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optManuDate.isPresent()) {
+            SubmodelElement element = optManuDate.get();
+
+            if(element instanceof Property p) {
+                return Optional.of(p.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of date of manufacture is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public HashMap<String, String> getHardwareVersion() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optHardwareVersion = getSubmodelElement(NameplateConstants.Nameplate2_0.hardwareVersion, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optHardwareVersion.isPresent()) {
+            SubmodelElement element = optHardwareVersion.get();
+
+            if(element instanceof MultiLanguageProperty mlp) {
+                return convertMlpToHashmap(mlp.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of hardware version is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    public HashMap<String, String> getFirmwareVersion() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optFirmwareVersion = getSubmodelElement(NameplateConstants.Nameplate2_0.firmwareVersion, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optFirmwareVersion.isPresent()) {
+            SubmodelElement element = optFirmwareVersion.get();
+
+            if(element instanceof MultiLanguageProperty mlp) {
+                return convertMlpToHashmap(mlp.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of firmware version is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    public HashMap<String, String> getSoftwareVersion() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optSoftwareVersion = getSubmodelElement(NameplateConstants.Nameplate2_0.softwareVersion, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optSoftwareVersion.isPresent()) {
+            SubmodelElement element = optSoftwareVersion.get();
+
+            if(element instanceof MultiLanguageProperty mlp) {
+                return convertMlpToHashmap(mlp.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of firmware version is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    public Optional<String> getCountryOfOrigin() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optOriginCountry = getSubmodelElement(NameplateConstants.Nameplate2_0.countryOfOrigin, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optOriginCountry.isPresent()) {
+            SubmodelElement element = optOriginCountry.get();
+
+            if(element instanceof Property p) {
+                return Optional.of(p.getValue());
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of country of origin is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<FileModel> getCompanyLogo() throws SubmodelElementNotFoundException {
+        Optional<SubmodelElement> optCompanyLogo = getSubmodelElement(NameplateConstants.Nameplate2_0.companyLogo, this.nameplateSubmodel.getSubmodelElements());
+
+        if(optCompanyLogo.isPresent()) {
+            SubmodelElement element = optCompanyLogo.get();
+
+            if(element instanceof File f) {
+                return Optional.of(FileModel.fromFileElement(f));
+            } else {
+                throw new SubmodelElementNotFoundException("Submodel element with semantic identifier of company logo is from unexpected type " + element.getClass().getName() + ".");
+            }
+        } else {
+            return Optional.empty();
         }
     }
 
