976da7229c4737758738ea2316f5f5ddea5307fc
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-15 09:10:43 +0100

    added various semantic identifiers for nameplate 2.0

--- src/main/java/org/simpleaas/nameplate/NameplateConstants.java
+++ src/main/java/org/simpleaas/nameplate/NameplateConstants.java
@@ -1,11 +1,133 @@
 package org.simpleaas.nameplate;
 
 public class NameplateConstants {
-    public static class nameplate2_0 {
-        public static String uriOfTheProduct = "";
+    public static class Nameplate2_0 {
+        public static String id = "";
+        public static String uriOfTheProduct = "0173-1#02-AAY811#001";
+        public static String manufacturerName = "0173-1#02-AAO677#002";
+        public static String manufacturerProductDesignation = "0173-1#02-AAW338#001";
+        public static String manufacturerProductRoot = "0173-1#02-AAU732#001";
+        public static String manufacturerProductFamily = "0173-1#02-AAU731#001";
+        public static String manufacturerProductType = "0173-1#02-AAO057#002";
+        public static String orderCodeOfManufacturer = "0173-1#02-AAO227#002";
+        public static String productArticleNumber = "0173-1#02-AAO676#003";
+        public static String serialNumber = "0173-1#02-AAM556#002";
+        public static String yearOfConstruction = "0173-1#02-AAP906#001";
+        public static String dateOfManufacture = "0173-1#02-AAR972#002";
+        public static String hardwareVersion = "0173-1#02-AAN270#002";
+        public static String firmwareVersion = "0173-1#02-AAM985#002";
+        public static String softwareVersion = "0173-1#02-AAM737#002";
+        public static String countryOfOrigin = "0173-1#02-AAO259#004";
+        public static String companyLogo = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/CompanyLogo";
+
+        public static class ContactInformation {
+            public static String id = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation";
+            public static String street = "0173-1#02-AAO128#002";
+            public static String zipcode = "0173-1#02-AAO129#002";
+            public static String cityTown = "0173-1#02-AAO132#002";
+            public static String nationalCode = "0173-1#02-AAO134#002";
+        }
+
+        public static class Markings {
+            public static String id = "0173-1#01-AGZ673#001";
+
+            public static class Marking {
+                public static String id = "0173-1#01-AHD206#001";
+                public static String markingName = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/MarkingName";
+                public static String designationOfCertificate = "0112/2///61987#ABH783#001";
+                public static String issueDate = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/IssueDate";
+                public static String expiryDate = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExpiryDate";
+                public static String markingFile = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/MarkingFile";
+                public static String markingAdditionalText = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/MarkingAdditionalText";
+
+                public static class ExplosionSafeties {
+                    public static String id = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties";
+
+                    public static class ExplosionSafety {
+                        public static String id = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety";
+                        public static String designationOfCertificateOrApproval = "0112/2///61987#ABH783#001";
+                        public static String typeOfApproval = "0173-1#02-AAM812#003";
+                        public static String approvalAgencyTestingAgency = "0173-1#02-AAM632#001";
+                        public static String typeOfProtection = "0173-1#02-AAQ325#003";
+                        public static String ratedInsulationVoltage = "0173-1#02-AAN532#003";
+                        public static String instructionsControlDrawing = "0112/2///61987#ABO102#001";
+                        public static String specificConditionsForUse = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/SpecificConditionsForUse";
+                        public static String incompleteDevice = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/IncompleteDevice";
+
+                        public static class AmbientConditions {
+                            public static String id = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/AmbientConditions";
+                            public static String deviceCategory = "0173-1#02-AAK297#004";
+                            public static String equipmentProtectionLevel = "0173-1#02-AAM668#001";
+                            public static String regionalSpecificMarking = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/RegionalSpecificMarking";
+                            public static String typeOfProtection = "0173-1#02-AAQ325#003";
+                            public static String explosionGroup = "0173-1#02-AAT372#001";
+                            public static String minimumAmbientTemperature = "0173-1#02-AAZ952#001";
+                            public static String maxAmbientTemperature = "0173-1#02-BAA039#010";
+                            public static String maxSurfaceTemperatureForDustProof = "0173-1#02-AAM666#005";
+                            public static String temperatureClass = "0173-1#02-AAO371#004";
+                        }
+
+                        public static class ProcessConditions {
+                            public static String id = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/ProcessConditions";
+                            public static String deviceCategory = "0173-1#02-AAK297#004";
+                            public static String equipmentProtectionLevel = "0173-1#02-AAM668#001";
+                            public static String regionalSpecificMarking = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/RegionalSpecific Marking";
+                            public static String typeOfProtection = "0173-1#02-AAQ325#003";
+                            public static String explosionGroup = "0173-1#02-AAT372#001";
+                            public static String lowerLimitingValueOfProcessTemperature = "0173-1#02-AAN309#004";
+                            public static String upperLimitingValueOfProcessTemperature = "0173-1#02-AAN307#004";
+                            public static String maxSurfaceTemperatureForDustProof = "0173-1#02-AAM666#005";
+                            public static String temperatureClass = "0173-1#02-AAO371#004";
+                        }
+
+                        public static class ExternalElectricalCircuit {
+                            public static String id = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/ExternalElectricalCircuit";
+                            public static String designationOfElectricalTerminal = "0112/2///61987#ABB147#004";
+                            public static String typeOfProtection = "0173-1#02-AAQ325#003";
+                            public static String equipmentProtectionLevel = "0173-1#02-AAM668#001";
+                            public static String explosionGroup = "0173-1#02-AAT372#001";
+                            public static String characteristics = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/ExternalElectric alCircuit/Characteristics";
+                            public static String fisco = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/ExternalElectricalCircuit/Fisco";
+                            public static String twoWise = "https://admin-shell.io/zvei/nameplate/2/0/Nameplate/Markings/Marking/ExplosionSafeties/ExplosionSafety/ExternalElectric alCircuit/TwoWISE";
+                            public static String safetyRelatedPropertiesForPassiveBehaviour = "0173-1#02-AAQ380#006";
+                            public static String safetyRelatedPropertiesForActiveBehaviour = "0173-1#02-AAQ381#006";
+
+                            public static class SafetyRelatedPropertiesForPassiveBehaviour {
+                                public static String id = "0173-1#02-AAQ380#006";
+                                public static String maxInputPower = "0173-1#02-AAQ372#003";
+                                public static String maxInputVoltage = "0173-1#02-AAM638#003";
+                                public static String maxInputCurrent = "0173-1#02-AAM642#004";
+                                public static String maxInternalCapacitance = "0173-1#02-AAM640#004";
+                                public static String maxInternalInductance = "0173-1#02-AAM639#003";
+                            }
+
+                            public static class SafetyRelatedPropertiesForActiveBehaviour {
+                                public static String id = "0173-1#02-AAQ381#006";
+                                public static String maxOutputPower = "0173-1#02-AAQ371#003";
+                                public static String maxOutputVoltage = "0173-1#02-AAM635#003";
+                                public static String maxOutputCurrent = "0173-1#02-AAM641#004";
+                                public static String maxEternalCapacitance = "0173-1#02-AAM637#004";
+                                public static String maxInternalInductance = "0173-1#02-AAM636#003";
+                                public static String maxExternalInductanceResistanceRatio = "0173-1#02-AAM634#003";
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static class AssetSpecificProperties {
+            public static String id = "0173-1#01-AGZ672#001";
+
+            public static class GuidelineSpecificProperties {
+                public static String id = "0173-1#01-AHD205#001";
+                public static String guidelineForConformityDeclaration = "0173-1#02-AAO856#002";
+            }
+        }
     }
 
-    public static class nameplate3_0 {
+    public static class Nameplate3_0 {
 
     }
 }
