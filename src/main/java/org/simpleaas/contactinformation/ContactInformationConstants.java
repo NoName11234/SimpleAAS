0f6f58bf5898cc5c099f0de079544946a61395df
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-21 23:23:06 +0100

    added semantic ids for submodel contact informations

---
+++ src/main/java/org/simpleaas/contactinformation/ContactInformationConstants.java
@@ -0,0 +1,59 @@
+package org.simpleaas.contactinformation;
+
+public class ContactInformationConstants {
+
+    public static class ContactInformations1_0 {
+        public static String id = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations";
+
+        public static class ContactInformation {
+            public static String id = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation";
+            public static String roleOfContactPerson = "0173-1#02-AAO204#003";
+            public static String nationalCode = "0173-1#02-AAO134#002";
+            public static String language = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/Language";
+            public static String timeZone = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/TimeZone";
+            public static String cityTown = "0173-1#02-AAO132#002";
+            public static String company = "0173-1#02-AAW001#001";
+            public static String department = "0173-1#02-AAO127#003";
+            public static String street = "0173-1#02-AAO128#002";
+            public static String zipcode = "0173-1#02-AAO129#002";
+            public static String poBox = "0173-1#02-AAO130#002";
+            public static String zipCodeOfPoBox = "0173-1#02-AAO131#002";
+            public static String stateCounty = "0173-1#02-AAO133#002";
+            public static String nameOfContact = "0173-1#02-AAO205#002";
+            public static String firstName = "0173-1#02-AAO206#002";
+            public static String middleNames = "0173-1#02-AAO207#002";
+            public static String title = "0173-1#02-AAO208#003";
+            public static String academicTitle = "0173-1#02-AAO209#003";
+            public static String furtherDetailsOfContact = "0173-1#02-AAO210#002";
+            public static String addressOfAdditionalLink = "0173-1#02-AAQ326#002";
+
+            public static class Phone {
+                public static String id = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/Phone";
+                public static String telephoneNumber = "0173-1#02-AAO136#002";
+                public static String typeOfTelephone = "0173-1#02-AAO137#003";
+                public static String availableTime = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/AvailableTime/";
+            }
+
+            public static class Fax {
+                public static String id = "0173-1#02-AAQ834#005";
+                public static String faxNumber = "0173-1#02-AAO195#002";
+                public static String typeOfFaxNumber = "0173-1#02-AAO196#003";
+            }
+
+            public static class Email {
+                public static String id = "0173-1#02-AAQ836#005";
+                public static String emailAddress = "0173-1#02-AAO198#002";
+                public static String publicKey = "0173-1#02-AAO200#002";
+                public static String typeOfEmailAddress = "0173-1#02-AAO199#003";
+                public static String typeOfPublicKey = "0173-1#02-AAO201#002";
+            }
+
+            public static class IpCommunication {
+                public static String id = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/IPCommunication";
+                public static String addressOfAdditionalLink = "0173-1#02-AAQ326#002";
+                public static String typeOfCommunication = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/IPCommunication/TypeOfCommunication";
+                public static String availableTime = "https://admin-shell.io/zvei/nameplate/1/0/ContactInformations/ContactInformation/AvailableTime/";
+            }
+        }
+    }
+}
