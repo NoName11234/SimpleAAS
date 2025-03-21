9991ff6532413fa368d5ff064316f1ca5ba553fa
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-22 00:00:13 +0100

    added and implemented model class for phone collections in contact informations

---
+++ src/main/java/org/simpleaas/contactinformation/contactinformation1_0/Phone.java
@@ -0,0 +1,50 @@
+package org.simpleaas.contactinformation.contactinformation1_0;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class Phone {
+    private final HashMap<String, String> telephoneNumber = new HashMap<>();
+    private String typeOfTelephone = null;
+    private HashMap<String, String> availableTime = new HashMap<>();
+
+    public Phone(String language, String telephoneNumber) {
+        this.telephoneNumber.put(language, telephoneNumber);
+    }
+
+    public Phone(HashMap<String, String> telephoneNumber) {
+        this.telephoneNumber.putAll(telephoneNumber);
+    }
+
+    public Phone addTelephoneNumber(String language, String telephoneNumber) {
+        this.telephoneNumber.put(language, telephoneNumber);
+        return this;
+    }
+
+    public HashMap<String, String> getTelephoneNumber() {
+        return this.telephoneNumber;
+    }
+
+    public Optional<String> getTypeOfTelephone() {
+        return Optional.ofNullable(this.typeOfTelephone);
+    }
+
+    public Phone setTypeOfTelephone(String typeOfTelephone) {
+        this.typeOfTelephone = typeOfTelephone;
+        return this;
+    }
+
+    public HashMap<String, String> getAvailableTime() {
+        return availableTime;
+    }
+
+    public Phone addAvailableTime(String language, String availableTime) {
+        this.availableTime.put(language, availableTime);
+        return this;
+    }
+
+    public Phone setAvailableTime(HashMap<String, String> availableTime) {
+        this.availableTime = availableTime;
+        return this;
+    }
+}
