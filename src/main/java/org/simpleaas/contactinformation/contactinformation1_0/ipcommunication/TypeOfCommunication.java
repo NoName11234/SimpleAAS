bd9852d8f4b716c4f4748c7b81fb9686ed7795d3
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-27 17:08:07 +0100

    finished implementation of enum TypeOfCommunication

--- src/main/java/org/simpleaas/contactinformation/contactinformation1_0/ipcommunication/TypeOfCommunication.java
+++ src/main/java/org/simpleaas/contactinformation/contactinformation1_0/ipcommunication/TypeOfCommunication.java
@@ -1,6 +1,33 @@
 package org.simpleaas.contactinformation.contactinformation1_0.ipcommunication;
 
+import java.util.Optional;
+
 public enum TypeOfCommunication {
-    Chat,
-    VideoCall;
+    Chat("Chat"),
+    VideoCall("Video call");
+
+    private final String type;
+
+    TypeOfCommunication(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return this.type;
+    }
+
+    public static Optional<TypeOfCommunication> createFromValue(String communicationValue) {
+
+        switch (communicationValue) {
+            case "Chat" -> {
+                return Optional.of(TypeOfCommunication.Chat);
+            }
+            case "Video call" -> {
+                return Optional.of(TypeOfCommunication.VideoCall);
+            }
+            default -> {
+             return Optional.empty();
+            }
+        }
+    }
 }
