d9267dfbe9d835f1a56a2f9ad642bb814f20d755
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-03-27 17:07:49 +0100

    implemented static method for getting TypeOfFaxNumber from semantic ID

--- src/main/java/org/simpleaas/contactinformation/contactinformation1_0/fax/TypeOfFaxNumber.java
+++ src/main/java/org/simpleaas/contactinformation/contactinformation1_0/fax/TypeOfFaxNumber.java
@@ -1,5 +1,7 @@
 package org.simpleaas.contactinformation.contactinformation1_0.fax;
 
+import java.util.Optional;
+
 public enum TypeOfFaxNumber {
     Office("0173-1#07-AAS754#001"),
     Secretary("0173-1#07-AAS756#001"),
@@ -13,5 +15,23 @@ public enum TypeOfFaxNumber {
 
     public String getSemanticId() {
         return this.type;
+    }
+
+    public static Optional<TypeOfFaxNumber> createFromSemanticId(String semanticId) {
+
+        switch (semanticId) {
+            case "0173-1#07-AAS754#001" -> {
+                return Optional.of(TypeOfFaxNumber.Office);
+            }
+            case "0173-1#07-AAS756#001" -> {
+                return Optional.of(TypeOfFaxNumber.Secretary);
+            }
+            case "0173-1#07-AAS758#001" -> {
+                return Optional.of(TypeOfFaxNumber.Home);
+            }
+            default -> {
+                return Optional.empty();
+            }
+        }
     }
 }
