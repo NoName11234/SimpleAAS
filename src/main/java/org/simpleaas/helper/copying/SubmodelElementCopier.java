c2cc76694c6f4955590accf49278af568b9ad2b2
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-13 16:14:29 +0200

    added method for creating copies of submodel elements in class SubmodelElementCopier

--- src/main/java/org/simpleaas/helper/copying/SubmodelElementCopier.java
+++ src/main/java/org/simpleaas/helper/copying/SubmodelElementCopier.java
@@ -1,6 +1,6 @@
 package org.simpleaas.helper.copying;
 
-import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
+import org.eclipse.digitaltwin.aas4j.v3.model.*;
 
 /**
  * Class for copying submodel elements.
@@ -16,5 +16,60 @@ public class SubmodelElementCopier {
      */
     public SubmodelElementCopier(SubmodelElement original) {
         this.original = original;
+    }
+
+    /**
+     * Creates a copy of the original submodel element.
+     * @return copied submodel element
+     */
+    private SubmodelElement createCopy() {
+        switch (original) {
+            case AnnotatedRelationshipElement annotatedRelationshipElement -> {
+
+            }
+            case RelationshipElement relationshipElement -> {
+
+            }
+            case Property property -> {
+
+            }
+            case MultiLanguageProperty multiLanguageProperty -> {
+
+            }
+            case Range range -> {
+
+            }
+            case Blob blob -> {
+
+            }
+            case File file -> {
+
+            }
+            case ReferenceElement referenceElement -> {
+
+            }
+            case Capability capability -> {
+
+            }
+            case SubmodelElementList submodelElementList -> {
+
+            }
+            case SubmodelElementCollection submodelElementCollection -> {
+
+            }
+            case Entity entity -> {
+
+            }
+            case BasicEventElement basicEventElement -> {
+
+            }
+            case Operation operation -> {
+
+            }
+            case OperationVariable operationVariable -> {
+
+            }
+            default -> throw new IllegalStateException("Unsupported submodel element sub type: " + original.getClass().getName());
+        }
     }
 }
