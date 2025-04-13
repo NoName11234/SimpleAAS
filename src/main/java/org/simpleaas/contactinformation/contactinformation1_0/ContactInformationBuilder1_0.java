bfa339fc31b7bb8a0485d393893fabb873d3d0a9
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-13 15:29:27 +0200

    created builder class for submodel Contact Information 1.0

---
+++ src/main/java/org/simpleaas/contactinformation/contactinformation1_0/ContactInformationBuilder1_0.java
@@ -0,0 +1,20 @@
+package org.simpleaas.contactinformation.contactinformation1_0;
+
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+
+/**
+ * Class for creating submodels of type Contact Information 1.0.
+ */
+public class ContactInformationBuilder1_0 {
+    /**
+     * Template submodel of type Contact Information.
+     */
+    private Submodel template;
+
+    /**
+     * @param submodel the template submodel which shall be used by the builder class
+     */
+    public ContactInformationBuilder1_0(Submodel submodel) {
+        this.template = submodel;
+    }
+}
