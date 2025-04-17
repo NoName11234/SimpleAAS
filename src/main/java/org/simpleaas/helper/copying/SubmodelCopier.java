8249d8b2ac7134e47439a0954f7d4c52c7134935
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-17 15:37:33 +0200

    implemented copying of submodels

--- src/main/java/org/simpleaas/helper/copying/SubmodelCopier.java
+++ src/main/java/org/simpleaas/helper/copying/SubmodelCopier.java
@@ -1,6 +1,8 @@
 package org.simpleaas.helper.copying;
 
-import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.aas4j.v3.model.*;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultAdministrativeInformation;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultSubmodel;
 
 /**
  * Class for copying submodels.
@@ -16,5 +18,83 @@ public class SubmodelCopier {
      */
     public SubmodelCopier(Submodel original) {
         this.original = original;
+    }
+
+    /**
+     * Creates a copy of the original submodel.
+     * @return copied submodel
+     */
+    public Submodel createCopy() {
+        var submodelBuilder = new DefaultSubmodel.Builder();
+
+        submodelBuilder.idShort(original.getIdShort());
+
+        for(LangStringNameType displayName: original.getDisplayName()) {
+            submodelBuilder.displayName(SubmodelElementCopier.copyLangStringNameType(displayName));
+        }
+
+        submodelBuilder.category(original.getCategory());
+
+        for(LangStringTextType desc: original.getDescription()) {
+            submodelBuilder.description(SubmodelElementCopier.copyLangStringTextType(desc));
+        }
+
+        submodelBuilder.id(original.getId());
+
+        if(original.getAdministration() != null) {
+            submodelBuilder.administration(copyAdministration(original.getAdministration()));
+        }
+
+        submodelBuilder.kind(original.getKind());
+
+        if(original.getSemanticId() != null) {
+            submodelBuilder.semanticId(SubmodelElementCopier.copyReference(original.getSemanticId()));
+        }
+
+        for(Reference supSemanticId: original.getSupplementalSemanticIds()) {
+            submodelBuilder.supplementalSemanticIds(SubmodelElementCopier.copyReference(supSemanticId));
+        }
+
+        for(Qualifier qualifier: original.getQualifiers()) {
+            submodelBuilder.qualifiers(SubmodelElementCopier.copyQualifier(qualifier));
+        }
+
+        for(EmbeddedDataSpecification specification: original.getEmbeddedDataSpecifications()) {
+            submodelBuilder.embeddedDataSpecifications(SubmodelElementCopier.copyEmbeddedDataSpecification(specification));
+        }
+
+        for(Extension extension: original.getExtensions()) {
+            submodelBuilder.extensions(SubmodelElementCopier.copyExtension(extension));
+        }
+
+        for(SubmodelElement submodelElement: original.getSubmodelElements()) {
+            SubmodelElementCopier copier = new SubmodelElementCopier(submodelElement);
+            submodelBuilder.submodelElements(copier.createCopy());
+        }
+
+        return submodelBuilder.build();
+    }
+
+    /**
+     * Creates a copy of an AdministrativeInformation.
+     * @param original the original AdministrativeInformation to be copied
+     * @return copied AdministrativeInformation
+     */
+    private AdministrativeInformation copyAdministration(AdministrativeInformation original) {
+        var adInfBuilder = new DefaultAdministrativeInformation.Builder();
+
+        adInfBuilder.version(original.getVersion());
+        adInfBuilder.revision(original.getRevision());
+        adInfBuilder.templateId(original.getTemplateId());
+
+        if(original.getCreator() != null) {
+            adInfBuilder.creator(SubmodelElementCopier.copyReference(original.getCreator()));
+        }
+
+        for(EmbeddedDataSpecification specification: original.getEmbeddedDataSpecifications()) {
+            adInfBuilder.embeddedDataSpecifications(SubmodelElementCopier.copyEmbeddedDataSpecification(specification));
+        }
+
+        return adInfBuilder.build();
     }
 }
