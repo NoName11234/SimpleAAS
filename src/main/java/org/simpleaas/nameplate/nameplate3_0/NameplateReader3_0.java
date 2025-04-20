11a89a8c1b02e32adf7577ba298fd57829de8554
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-20 21:05:35 +0200

    added empty methods to reader for digital nameplate 3.0

--- src/main/java/org/simpleaas/nameplate/nameplate3_0/NameplateReader3_0.java
+++ src/main/java/org/simpleaas/nameplate/nameplate3_0/NameplateReader3_0.java
@@ -1,4 +1,98 @@
 package org.simpleaas.nameplate.nameplate3_0;
 
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.simpleaas.contactinformation.contactinformation1_0.ContactInformation;
+import org.simpleaas.helper.FileModel;
+import org.simpleaas.nameplate.nameplate3_0.assetspecificproperty.AssetSpecificProperty;
+import org.simpleaas.nameplate.nameplate3_0.marking.Marking;
+
+import java.util.HashMap;
+import java.util.Optional;
+
 public class NameplateReader3_0 {
+    private final Submodel submodel;
+
+    public NameplateReader3_0(Submodel submodel) {
+        this.submodel = submodel;
+    }
+
+    public String getUriOfTheProduct() {
+
+    }
+
+    public HashMap<String, String> getManufacturerName() {
+
+    }
+
+    public HashMap<String, String> getManufacturerProductDesignation() {
+
+    }
+
+    public ContactInformation getAddressInformation() {
+
+    }
+
+    public HashMap<String, String> getManufacturerProductRoot() {
+
+    }
+
+    public HashMap<String, String> getManufacturerProductFamily() {
+
+    }
+
+    public Optional<String> getManufacturerProductType() {
+
+    }
+
+    public String getOrderCodeOfManufacturer() {
+
+    }
+
+    public Optional<String> getProductArticleNumberOfManufacturer() {
+
+    }
+
+    public Optional<String> getSerialNumber() {
+
+    }
+
+    public Optional<String> getYearOfConstruction() {
+
+    }
+
+    public Optional<String> getDateOfManufacture() {
+
+    }
+
+    public Optional<String> getHardwareVersion() {
+
+    }
+
+    public Optional<String> getFirmwareVersion() {
+
+    }
+
+    public Optional<String> getSoftwareVersion() {
+
+    }
+
+    public Optional<String> getCountryOfOrigin() {
+
+    }
+
+    public Optional<String> getUniqueFacilityIdentifier() {
+
+    }
+
+    public Optional<FileModel> getCompanyLogo() {
+
+    }
+
+    public HashMap<String, Marking> getMarkings() {
+
+    }
+
+    public HashMap<String, AssetSpecificProperty> getAssetSpecificProperties() {
+        
+    }
 }
