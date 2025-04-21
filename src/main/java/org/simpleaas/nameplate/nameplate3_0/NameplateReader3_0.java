87aee2338af4d82e0da0a873e58648a6fad15425
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-21 21:12:38 +0200

    added internal helper methods

--- src/main/java/org/simpleaas/nameplate/nameplate3_0/NameplateReader3_0.java
+++ src/main/java/org/simpleaas/nameplate/nameplate3_0/NameplateReader3_0.java
@@ -1,12 +1,13 @@
 package org.simpleaas.nameplate.nameplate3_0;
 
-import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.aas4j.v3.model.*;
 import org.simpleaas.contactinformation.contactinformation1_0.ContactInformation;
 import org.simpleaas.helper.FileModel;
 import org.simpleaas.nameplate.nameplate3_0.assetspecificproperty.AssetSpecificProperty;
 import org.simpleaas.nameplate.nameplate3_0.marking.Marking;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public class NameplateReader3_0 {
@@ -93,6 +94,61 @@ public class NameplateReader3_0 {
     }
 
     public HashMap<String, AssetSpecificProperty> getAssetSpecificProperties() {
-        
+
+    }
+
+    private boolean compareSemanticId(Reference reference, String semanticId) {
+        for(Key key: reference.getKeys()) {
+            if(key.getValue().equals(semanticId)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Optional<String> getPropertyValue(List<SubmodelElement> elements, String semanticId) {
+        return elements.stream()
+                .filter(se -> se instanceof Property)
+                .map(se -> ((Property)se))
+                .filter(p -> compareSemanticId(p.getSemanticId(), semanticId))
+                .map(Property::getValue)
+                .findAny();
+    }
+
+    private HashMap<String, String> getMlpValue(List<SubmodelElement> elements, String semanticId) {
+        return elements.stream()
+                .filter(se -> se instanceof MultiLanguageProperty)
+                .map(se -> ((MultiLanguageProperty)se))
+                .filter(mlp -> compareSemanticId(mlp.getSemanticId(), semanticId))
+                .map(this::convertMlp)
+                .findAny().orElse(new HashMap<>());
+    }
+
+    private Optional<SubmodelElementCollection> getSmc(List<SubmodelElement> elements, String semanticId) {
+        return elements.stream()
+                .filter(se -> se instanceof SubmodelElementCollection)
+                .map(se -> ((SubmodelElementCollection)se))
+                .filter(smc -> compareSemanticId(smc.getSemanticId(), semanticId))
+                .findAny();
+    }
+
+    private Optional<FileModel> getFileModel(List<SubmodelElement> elements, String semanticId) {
+        return elements.stream()
+                .filter(se -> se instanceof File)
+                .map(se -> ((File)se))
+                .filter(f -> compareSemanticId(f.getSemanticId(), semanticId))
+                .map(f -> new FileModel(f.getValue(), f.getContentType()))
+                .findAny();
+    }
+
+    private HashMap<String, String> convertMlp(MultiLanguageProperty mlp) {
+        HashMap<String, String> values = new HashMap<>();
+
+        for(LangStringTextType value: mlp.getValue()) {
+            values.put(value.getLanguage(), value.getText());
+        }
+
+        return values;
     }
 }
