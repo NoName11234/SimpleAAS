90de540dea265ba66b71e5fa646969c5bbc18bc5
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-22 21:15:27 +0200

    implemented data class for GuidelineSpecificPropertyCollections

--- src/main/java/org/simpleaas/nameplate/nameplate2_0/assetspecificproperty/guidelinespecificproperty/GuidelineSpecificPropertyCollection.java
+++ src/main/java/org/simpleaas/nameplate/nameplate2_0/assetspecificproperty/guidelinespecificproperty/GuidelineSpecificPropertyCollection.java
@@ -1,4 +1,29 @@
 package org.simpleaas.nameplate.nameplate2_0.assetspecificproperty.guidelinespecificproperty;
 
+import java.util.HashMap;
+
 public class GuidelineSpecificPropertyCollection {
+    private final String guidelineForConformityDeclaration;
+    private final HashMap<String, String> arbitraryProperties = new HashMap<>();
+
+    public GuidelineSpecificPropertyCollection(String guidelineForConformityDeclaration) {
+        this.guidelineForConformityDeclaration = guidelineForConformityDeclaration;
+    }
+
+    public String getGuidelineForConformityDeclaration() {
+        return this.guidelineForConformityDeclaration;
+    }
+
+    public GuidelineSpecificPropertyCollection addArbitraryProperty(String shortId, String value) {
+        this.arbitraryProperties.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getArbitraryProperties() {
+        return this.arbitraryProperties;
+    }
+
+    public boolean areArbitraryPropertiesPresent() {
+        return !this.arbitraryProperties.isEmpty();
+    }
 }
