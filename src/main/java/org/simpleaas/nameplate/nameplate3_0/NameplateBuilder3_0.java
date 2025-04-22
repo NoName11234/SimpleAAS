3c7af60484a72e7c5fb8cebbf5b89ccba46a9ebd
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-22 14:01:58 +0200

    added class NameplateBuilder3_0

---
+++ src/main/java/org/simpleaas/nameplate/nameplate3_0/NameplateBuilder3_0.java
@@ -0,0 +1,11 @@
+package org.simpleaas.nameplate.nameplate3_0;
+
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+
+public class NameplateBuilder3_0 {
+    private Submodel template;
+
+    public NameplateBuilder3_0(Submodel template) {
+        
+    }
+}
