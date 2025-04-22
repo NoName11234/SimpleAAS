97cbeed8cec3cfd5791ed9b6842ed9b0bf932352
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-04-22 20:38:54 +0200

    implemented data class for asset specific property collections

--- src/main/java/org/simpleaas/nameplate/nameplate2_0/assetspecificproperty/AssetSpecificPropertyCollection.java
+++ src/main/java/org/simpleaas/nameplate/nameplate2_0/assetspecificproperty/AssetSpecificPropertyCollection.java
@@ -1,4 +1,36 @@
 package org.simpleaas.nameplate.nameplate2_0.assetspecificproperty;
 
+import org.simpleaas.nameplate.nameplate2_0.assetspecificproperty.guidelinespecificproperty.GuidelineSpecificPropertyCollection;
+
+import java.util.HashMap;
+
 public class AssetSpecificPropertyCollection {
+    private final HashMap<String, String> arbitraryProperties = new HashMap<>();
+    private final HashMap<String, GuidelineSpecificPropertyCollection> guidelineSpecificPropertyCollections = new HashMap<>();
+
+    public AssetSpecificPropertyCollection addArbitraryProperty(String shortId, String value) {
+        this.arbitraryProperties.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getArbitraryProperties() {
+        return this.arbitraryProperties;
+    }
+
+    public boolean areArbitraryPropertiesPresent() {
+        return !this.arbitraryProperties.isEmpty();
+    }
+
+    public AssetSpecificPropertyCollection addGuidelineSpecificPropertyCollection(String shortId, GuidelineSpecificPropertyCollection collection) {
+        this.guidelineSpecificPropertyCollections.put(shortId, collection);
+        return this;
+    }
+
+    public HashMap<String, GuidelineSpecificPropertyCollection> getGuidelineSpecificPropertyCollections() {
+        return this.guidelineSpecificPropertyCollections;
+    }
+
+    public boolean areGuidelineSpecificPropertyCollectionsPresent() {
+        return !this.guidelineSpecificPropertyCollections.isEmpty();
+    }
 }
