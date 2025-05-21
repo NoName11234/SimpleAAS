855fe6b093b63b09e7adb0bf37c0002cc1c89ed0
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 22:18:09 +0200

    added data model class for rectangular bar data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RectangularBar.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RectangularBar.java
@@ -1,4 +1,41 @@
 package org.simpleaas.inspectiondocumentssteelproducts.productdata;
 
+import java.util.HashMap;
+
 public class RectangularBar {
+    private final String height;
+    private final String width;
+    private final String length;
+    private final HashMap<String, String> standardReferences = new HashMap<>();
+
+    public RectangularBar(String height, String width, String length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public RectangularBar addStandardReference(String shortId, String value) {
+        this.standardReferences.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getStandardReferences() {
+        return this.standardReferences;
+    }
+
+    public boolean areStandardReferencesPresent() {
+        return !this.standardReferences.isEmpty();
+    }
 }
