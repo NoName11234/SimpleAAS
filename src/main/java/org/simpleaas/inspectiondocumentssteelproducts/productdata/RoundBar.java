ede01bba43649512c6132b5fecd728b62041ca78
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 22:14:19 +0200

    added data model class for round bar data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RoundBar.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RoundBar.java
@@ -1,4 +1,34 @@
 package org.simpleaas.inspectiondocumentssteelproducts.productdata;
 
+import java.util.HashMap;
+
 public class RoundBar {
+    private final String outerDiameter;
+    private final String length;
+    private final HashMap<String, String> standardReferences = new HashMap<>();
+
+    public RoundBar(String outerDiameter, String length) {
+        this.outerDiameter = outerDiameter;
+        this.length = length;
+    }
+
+    public String getOuterDiameter() {
+        return outerDiameter;
+    }
+    public String getLength() {
+        return length;
+    }
+
+    public RoundBar addStandardReference(String shortId, String value) {
+        this.standardReferences.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getStandardReferences() {
+        return this.standardReferences;
+    }
+
+    public boolean areStandardReferencesPresent() {
+        return !this.standardReferences.isEmpty();
+    }
 }
