f11d4c4f4472ab7cb63781e7b30ca0c3e83e02e2
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 22:29:14 +0200

    added data model class for profile data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/Profile.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/Profile.java
@@ -1,4 +1,47 @@
 package org.simpleaas.inspectiondocumentssteelproducts.productdata;
 
+import java.util.HashMap;
+
 public class Profile {
+    private final String profileSpecification;
+    private final String height;
+    private final String width;
+    private final String length;
+    private final HashMap<String, String> standardReferences = new HashMap<>();
+
+    public Profile(String profileSpecification, String height, String width, String length) {
+        this.profileSpecification = profileSpecification;
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getProfileSpecification() {
+        return profileSpecification;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public Profile addStandardReference(String shortId, String value) {
+        this.standardReferences.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getStandardReferences() {
+        return this.standardReferences;
+    }
+
+    public boolean areStandardReferencesPresent() {
+        return !this.standardReferences.isEmpty();
+    }
 }
