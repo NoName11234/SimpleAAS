51f53fcc342c9fea1657719da175672758441bba
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 21:34:32 +0200

    added data model class for rectangular tube data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RectangularTube.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/RectangularTube.java
@@ -1,4 +1,47 @@
 package org.simpleaas.inspectiondocumentssteelproducts.productdata;
 
+import java.util.HashMap;
+
 public class RectangularTube {
+    private final String wallThickness;
+    private final String height;
+    private final String width;
+    private final String length;
+    private final HashMap<String, String> standardReferences = new HashMap<>();
+
+    public RectangularTube(String wallThickness, String height, String width, String length) {
+        this.wallThickness = wallThickness;
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getWallThickness() {
+        return wallThickness;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public RectangularTube addStandardReference(String shortId, String value) {
+        this.standardReferences.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getStandardReferences() {
+        return this.standardReferences;
+    }
+
+    public boolean areStandardReferencesPresent() {
+        return !this.standardReferences.isEmpty();
+    }
 }
