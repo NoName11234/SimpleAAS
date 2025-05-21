e0ac5cd7f2e043e8aeec8fc5ca88749ae579451c
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 22:16:03 +0200

    added data model class for hexagonal bar data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/HexagonalBar.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/productdata/HexagonalBar.java
@@ -1,4 +1,35 @@
 package org.simpleaas.inspectiondocumentssteelproducts.productdata;
 
+import java.util.HashMap;
+
 public class HexagonalBar {
+    private final String widthAcrossFlats;
+    private final String length;
+    private final HashMap<String, String> standardReferences = new HashMap<>();
+
+    public HexagonalBar(String widthAcrossFlats, String length) {
+        this.widthAcrossFlats = widthAcrossFlats;
+        this.length = length;
+    }
+
+    public String getWidthAcrossFlats() {
+        return widthAcrossFlats;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public HexagonalBar addStandardReference(String shortId, String value) {
+        this.standardReferences.put(shortId, value);
+        return this;
+    }
+
+    public HashMap<String, String> getStandardReferences() {
+        return this.standardReferences;
+    }
+
+    public boolean areStandardReferencesPresent() {
+        return !this.standardReferences.isEmpty();
+    }
 }
