a8d212229779ff72889482836080000d9eacc9d6
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 17:35:09 +0200

    added data model class for manufacturer data

---
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/Manufacturer.java
@@ -0,0 +1,49 @@
+package org.simpleaas.inspectiondocumentssteelproducts;
+
+import java.util.HashMap;
+
+public class Manufacturer {
+    private final String companyName;
+    private final String street;
+    private final String zipCode;
+    private final String city;
+    private final String nationalCode;
+    private final HashMap<String, String> mailAddresses = new HashMap<>();
+
+    public Manufacturer(String companyName, String street, String zipCode, String city, String nationalCode) {
+        this.companyName = companyName;
+        this.street = street;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.nationalCode = nationalCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public HashMap<String, String> getMailAddresses() {
+        return mailAddresses;
+    }
+
+    public Manufacturer addMailAddress(String shortId, String mailAddress) {
+        this.mailAddresses.put(shortId, mailAddress);
+        return this;
+    }
+}
