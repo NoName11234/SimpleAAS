dc32635ddb3ec7e1e8266de89183a432f46aa051
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2025-05-21 18:09:48 +0200

    added data model class for customer data

--- src/main/java/org/simpleaas/inspectiondocumentssteelproducts/Customer.java
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/Customer.java
@@ -2,15 +2,18 @@ package org.simpleaas.inspectiondocumentssteelproducts;
 
 import java.util.HashMap;
 
-public class Manufacturer {
+public class Customer {
+    private final String shortId;
     private final String companyName;
+    private final HashMap<String, String> customerRoles = new HashMap<>();
     private final String street;
     private final String zipCode;
     private final String city;
     private final String nationalCode;
     private final HashMap<String, String> mailAddresses = new HashMap<>();
 
-    public Manufacturer(String companyName, String street, String zipCode, String city, String nationalCode) {
+    public Customer(String shortId, String companyName, String street, String zipCode, String city, String nationalCode) {
+        this.shortId = shortId;
         this.companyName = companyName;
         this.street = street;
         this.zipCode = zipCode;
@@ -18,8 +21,25 @@ public class Manufacturer {
         this.nationalCode = nationalCode;
     }
 
+    public String getShortId() {
+        return shortId;
+    }
+
     public String getCompanyName() {
         return companyName;
+    }
+
+    public Customer addCustomerRole(String shortId, String customerRole) {
+        this.customerRoles.put(shortId, customerRole);
+        return this;
+    }
+
+    public HashMap<String, String> getCustomerRoles() {
+        return customerRoles;
+    }
+
+    public boolean areCustomerRolesPresent() {
+        return !this.customerRoles.isEmpty();
     }
 
     public String getStreet() {
@@ -38,13 +58,13 @@ public class Manufacturer {
         return nationalCode;
     }
 
-    public HashMap<String, String> getMailAddresses() {
-        return mailAddresses;
-    }
-
-    public Manufacturer addMailAddress(String shortId, String mailAddress) {
+    public Customer addMailAddress(String shortId, String mailAddress) {
         this.mailAddresses.put(shortId, mailAddress);
         return this;
+    }
+
+    public HashMap<String, String> getMailAddresses() {
+        return mailAddresses;
     }
 
     public boolean areMailAddressesPresent() {
