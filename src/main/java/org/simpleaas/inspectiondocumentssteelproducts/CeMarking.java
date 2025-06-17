8ff46913a63ec1be67492a4f1d9e53c3212c90a5
NoName11234 <ykonto91@gmail.com>
2025-06-17 18:03:05 +0200

    implemented data class for validation and CE marking

---
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/CeMarking.java
@@ -0,0 +1,64 @@
+package org.simpleaas.inspectiondocumentssteelproducts;
+
+import org.simpleaas.helper.FileModel;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class CeMarking {
+    private final String shortId;
+    private final String markingName;
+    private String designationOfCertificateOrApproval = null;
+    private String issueDate = null;
+    private final FileModel markingFile;
+    private final HashMap<String, String> markingAdditionalTexts = new HashMap<>();
+
+    public CeMarking(String shortId, String markingName, FileModel markingFile) {
+        this.shortId = shortId;
+        this.markingName = markingName;
+        this.markingFile = markingFile;
+    }
+
+    public String getShortId() {
+        return this.shortId;
+    }
+
+    public String getMarkingName() {
+        return this.markingName;
+    }
+
+    public CeMarking setDesignationOfCertificateOrApproval(String designationOfCertificateOrApproval) {
+        this.designationOfCertificateOrApproval = designationOfCertificateOrApproval;
+        return this;
+    }
+
+    public Optional<String> getDesignationOfCertificateOrApproval() {
+        return Optional.ofNullable(this.designationOfCertificateOrApproval);
+    }
+
+    public CeMarking setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+        return this;
+    }
+
+    public Optional<String> getIssueDate() {
+        return Optional.ofNullable(this.issueDate);
+    }
+
+    public FileModel getMarkingFile() {
+        return this.markingFile;
+    }
+
+    public CeMarking addMarkingAdditionalText(String shortId, String markingAdditionalText) {
+        this.markingAdditionalTexts.put(shortId, markingAdditionalText);
+        return this;
+    }
+
+    public HashMap<String, String> getMarkingAdditionalTexts() {
+        return this.markingAdditionalTexts;
+    }
+
+    public boolean areMarkingAdditionalTextsPresent() {
+        return !this.markingAdditionalTexts.isEmpty();
+    }
+}
---
+++ src/main/java/org/simpleaas/inspectiondocumentssteelproducts/Validation.java
@@ -0,0 +1,42 @@
+package org.simpleaas.inspectiondocumentssteelproducts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Validation {
+    private final String statementOfCompliance;
+    private final String dateOfIssue;
+    private final String originatorOfStatement;
+    private final List<CeMarking> ceMarkings = new ArrayList<>();
+
+    public Validation(String statementOfCompliance, String dateOfIssue, String originatorOfStatement) {
+        this.statementOfCompliance = statementOfCompliance;
+        this.dateOfIssue = dateOfIssue;
+        this.originatorOfStatement = originatorOfStatement;
+    }
+
+    public String getStatementOfCompliance() {
+        return this.statementOfCompliance;
+    }
+
+    public String getDateOfIssue() {
+        return this.dateOfIssue;
+    }
+
+    public String getOriginatorOfStatement() {
+        return this.originatorOfStatement;
+    }
+
+    public Validation addCeMarking(CeMarking ceMarking) {
+        this.ceMarkings.add(ceMarking);
+        return this;
+    }
+
+    public List<CeMarking> getCeMarkings() {
+        return this.ceMarkings;
+    }
+
+    public boolean areCeMarkingsPresent() {
+        return !this.ceMarkings.isEmpty();
+    }
+}
